3acea91ac2baa1e491210bceb4e47095e58dcb8f
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-03-03 14:37:22 -0300

    att

---
+++ biblioteca_db.sql
@@ -0,0 +1,9 @@
+create table login(
+	idusuario int not null auto_increment,
+	nome text,
+    telefone text,
+    endereco text,
+    usuario text,
+    senha text,
+    primary key (idusuario)
+);
