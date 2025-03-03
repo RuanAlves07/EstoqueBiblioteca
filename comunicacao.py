6a0a0f267c7f9824cbd8e21abeec63215ad2ddf5
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-03-03 15:24:27 -0300

    att

---
+++ comunicacao.py
@@ -0,0 +1,3 @@
+import mysql.connector
+from config import MYSQL_DATABASE, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_USER
+
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'root'
 MYSQL_DATABASE = 'biblioteca_db'
+
+#pip install mysql-connector-python
