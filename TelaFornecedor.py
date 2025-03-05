96684e50db95f3e823f8358c4e0ce14bcb02199d
Gabriel Bartholdy <gabrielbartholdy07@gmail.com>
2025-03-05 20:19:24 -0300

    Create TelaFornecedor.py

---
+++ TelaFornecedor.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+
+# Criar a janela
+jan = Tk()
+jan.title("Fornecedor")
+jan.geometry("400x200")
+jan.configure(background="white")
+jan.resizable(width=False, height=False)
