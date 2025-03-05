2753f3cb20de35016dcf9185850e6c9ec2e15ab9
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-03-05 19:52:39 -0300

    att

--- biblioteca_db.sql
+++ biblioteca_db.sql
@@ -1,9 +1,34 @@
 create table login(
+
 	idusuario int not null auto_increment,
 	nome text,
-    telefone text,
-    endereco text,
     usuario text,
     senha text,
+    endereco text,
+    telefone text,
     primary key (idusuario)
+
+);
+
+create table produtos(
+
+    idproduto int not null auto_increment,
+    nome text,
+    descricao text,
+    genero text,
+    quantidade text,
+    preco text,
+    primary key (idproduto)
+
+);
+
+create table fornecedor(
+
+    idfornecedor int not null auto_increment,
+    nome text,
+    nomefantasia text,
+    CNPJ text,
+    endereco text,
+    primary key (idfornecedor)
+
 );
