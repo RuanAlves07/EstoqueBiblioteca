79395ff615254c0c31c89e2b0991bb897f1cc9bc
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-03-05 20:07:00 -0300

    att

--- comunicacao.py
+++ comunicacao.py
@@ -1,3 +1,10 @@
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'root'
+MYSQL_DATABASE = 'biblioteca_db'
+
+#pip install mysql-connector-python
+
 import mysql.connector
 from config import MYSQL_DATABASE, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_USER
 
