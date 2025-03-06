f70df1354e0d05d43c5ab6834a23f711846b9021
Gabriel Bartholdy <gabrielbartholdy07@gmail.com>
2025-03-06 19:22:03 -0300

    Update fornecedor.py

--- fornecedor.py
+++ fornecedor.py
@@ -2,12 +2,12 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 
-# eu quero gozar
+
 # Criar a janela
 jan = Tk()
 jan.title("Fornecedor")
 jan.geometry("400x600")
-jan.configure(background="#808080")
+jan.configure(background="#f6f3ec")
 jan.resizable(width=False, height=False)
 
 
@@ -15,34 +15,57 @@ def cadastro_forn():
     jan = Tk()
     jan.title("Cadastro de Fornecedores")
     jan.geometry("400x600")
-    jan.configure(background="#808080")
+    jan.configure(background="#f6f3ec")
     jan.resizable(width=False, height=False)
-    UsuarioEntry = ttk.Entry(jan, width = 30) # Criar um campo de entrada para o usuário
-    UsuarioEntry.place (x = 120, y = 115)
+
+
+
+    fornecedores_label = Label(jan, text="NOME EMPRESARIAL: ", font=("Arial",13))
+    fornecedores_label.place(x = 120, y = 85)  # Centraliza o label na tela
+    fornecedoEntry = ttk.Entry(jan, width = 40) # Criar um campo de entrada para o usuário
+    fornecedoEntry.place (x = 80, y = 115)
+
+    fornecedores_ficticio = Label(jan, text="NOME DE FANTASIA: ", font=("Arial",13))
+    fornecedores_ficticio.place(x = 120, y = 160)  # Centraliza o label na tela
+    ficticioEntry = ttk.Entry(jan, width = 40) # Criar um campo de entrada para o usuário
+    ficticioEntry.place (x = 80, y = 190)
+
+
     voltButton = ttk.Button(jan, text = "Fechar", width = 10,command=jan.withdraw) # Cria um botão 
     voltButton.place(x = 10, y = 570)
 def excluir_forn():
     jan = Tk()
     jan.title("Exclusão de Fornecedores")
     jan.geometry("400x600")
-    jan.configure(background="#808080")
+    jan.configure(background="#f6f3ec")
     jan.resizable(width=False, height=False)
+
+    UsuarioEntry = ttk.Entry(jan, width = 40) # Criar um campo de entrada para o usuário
+    UsuarioEntry.place (x = 80, y = 115)
+
+    voltButton = ttk.Button(jan, text = "Fechar", width = 10,command=jan.withdraw) # Cria um botão 
+    voltButton.place(x = 10, y = 570)
+
 
 
 def listar_forn():
     jan = Tk()
     jan.title("lista de Fornecedores")
     jan.geometry("400x600")
-    jan.configure(background="#808080")
+    jan.configure(background="#f6f3ec")
     jan.resizable(width=False, height=False)
 
+    voltButton = ttk.Button(jan, text = "Fechar", width = 10,command=jan.withdraw) # Cria um botão 
+    voltButton.place(x = 10, y = 570)
 def atuu_funci():
     jan = Tk()
     jan.title("Atualizar Fornecedores")
     jan.geometry("400x600")
-    jan.configure(background="#808080")
+    jan.configure(background="#f6f3ec")
     jan.resizable(width=False, height=False)
-    
+
+    voltButton = ttk.Button(jan, text = "Fechar", width = 10,command=jan.withdraw) # Cria um botão 
+    voltButton.place(x = 10, y = 570)
 
 def sair():
     jan.withdraw()
