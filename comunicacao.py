529385c084b0299d4facf2e4102790324ba13e72
Isaac <isaac_s_souza@estudante.sesisenai.org.br>
2025-03-07 20:50:03 -0300

    ok

--- comunicacao.py
+++ comunicacao.py
@@ -2,7 +2,6 @@
 
 import mysql.connector
 
-
 #Login
 class login:
     def __init__(self):
@@ -13,13 +12,3 @@ class login:
             database = "biblioteca_db"
         )
         self.cursor = self.conn.cursor() 
-
-    def RegistrarNoBanco(self, nome, senha, email, telefone):
-        self.cursor.execute("INSERT INTO usuario (nome, senha, email, telefone) VALUES (%s, %s, %s, %s)", (nome, senha, email, telefone)) 
-        self.conn.commit()
-
-    
-        
-
-
-
