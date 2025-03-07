aef43decc4bdd6113d01fcbd986e04043c76e5fb
Isaac <isaac_s_souza@estudante.sesisenai.org.br>
2025-03-06 21:29:29 -0300

    att

--- comunicacao.py
+++ comunicacao.py
@@ -3,3 +3,21 @@
 import mysql.connector
 from config import MYSQL_DATABASE, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_USER
 
+#Login
+class login:
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "",
+            database = "biblioteca_db"
+        )
+        self.cursor = self.conn.cursor() 
+
+    def RegistrarNoBanco(self, nome, senha, email, telefone):
+        self.cursor.execute("INSERT INTO usuario (nome, senha, email, telefone) VALUES (%s, %s, %s, %s)", (nome, senha, email, telefone)) 
+        self.conn.commit()
+
+    def buscar(self, idUsuario):
+        self.cursor.execute("SELECT * FROM usuario WHERE idUsuario = 2", (idUsuario))
+        from Menu import jan
